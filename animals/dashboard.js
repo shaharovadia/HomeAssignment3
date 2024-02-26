a558a1b21695d3538a779891b3b10b79ed3ca936
shaharovadia <shaharovadia66@gmail.com>
2024-02-26 17:45:48 +0200

    dashboardFirst

--- animals/dashboard.js
+++ animals/dashboard.js
@@ -1,9 +1,48 @@
-function showVisitedAnimals() {
+function showVisitedAnimals(visitor) {
   //ממשו את הלוגיקה שמציגה את החיות שהאורח הנוכחי ביקר בהן
+  if (!visitor) {
+    console.log("Visitor not found.");
+    return false;
+  }
+    if (visitor.visitedAnimals.length === 0) {
+      console.log(visitor.name, "has not visited any animals yet.");
+      return false;}
+
+      console.log("Visited animals for", visitor.name, ":");
+  for (const animal of visitor.visitedAnimals) {
+    console.log("- ", animal.name);
+  }
 }
-function showFeededAnimals() {
+function showFeededAnimals(visitor) {
   //ממשו את הלוגיקה שמציגה את החיות שהאורח הנוכחי האכיל אותן
+  if (!visitor) {
+    console.log("Visitor not found.");
+    return false;
+  }
+    if (visitor.FeededAnimals.length === 0) {
+      console.log(visitor.name, "has not feeded any animals yet.");
+      return false;}
+
+    console.log("feeded animals for", visitor.name, ":");
+    for (const animal of visitor.FeededAnimals) {
+      console.log("- ", animal.name);
+    }
+
+
 }
-function showFavoriteAnimal() {
+function showFavoriteAnimal(visitor) {
   //ממשו את הלוגיקה שמציגה את החיה שהאורח ביקר הכי הרבה פעמים אצלה
+  if (!visitor) {
+    console.log("Visitor not found.");
+    return false;
+  }
+
+  if (visitor.visitedAnimals.length === 0) {
+    console.log(visitor.name, "has not visited any animals yet.");
+    return false;
+  }
+
+  
+
+
 }
--- animals/signup.js
+++ animals/signup.js
@@ -1,11 +1,11 @@
 const form = document.getElementById("create-visitor-form");
 const firstNameInput = document.getElementById("fname");
 const lastNameInput = document.getElementById("lname");
-const fullName = firstNameInput.value + " " + lastNameInput.value;
+const fullName = firstNameInput.value.concat(" ", lastNameInput.value);
 
 document.getElementById("submit").addEventListener("click", (event) => {
   event.preventDefault();
-  const fullName = firstNameInput.value + " " + lastNameInput.value;
+  const fullName = firstNameInput.value.concat(" ", lastNameInput.value);
   createNewVisitor(fullName);
 });
 
@@ -15,11 +15,11 @@ function createNewVisitor(fullName) {
   }
 
   if(!visitorExists(fullName)){
-  return false;
+    return false;
   } 
+
   else{
     makeVisitor(fullName);
-  
   }
 }
 
@@ -52,18 +52,15 @@ function createNewVisitor(fullName) {
   }
 
    const makeVisitor = (fullName) => {
-    // מקבל שם, בודק שאין אותו כבר במערך האורחים ומחזיר אובייקט אורח
-    let visitor = {name: fullName , coins: 50}
-  visitors.push(visitor);
-  const visitorsJson = JSON.stringify(visitors);
-
-  // Save the JSON string in local storage using the key "visitors"
-  localStorage.setItem("visitors", visitorsJson);
-
-  // Display success message or perform other actions (optional)
-  alert("Visitor added successfully!");
-  form.reset(); // Reset the form after successful submission
-  window.location.href="./login.html";
+       // מקבל שם, בודק שאין אותו כבר במערך האורחים ומחזיר אובייקט אורח
+      let visitor = {name: fullName , coins: 50, visitedAnimals : []}
+      visitors.push(visitor);
+      const visitorsJson = JSON.stringify(visitors);
+       // Save the JSON string in local storage using the key "visitors"
+      localStorage.setItem("visitors", visitorsJson);
+      alert("Visitor added successfully!");
+      form.reset(); // Reset the form after successful submission
+      window.location.href="./login.html";
    }
  
 /**************************************
@@ -71,7 +68,7 @@ function createNewVisitor(fullName) {
   שימו לב כי האיידי של createForm
   זהה לאיידי של הטופס בעמוד signup.html
   אין לשנות אותו */
-const createForm = document.getElementById("create-visitor-form");
-if (createForm) {
-  createForm.addEventListener("submit", createNewVisitor);
-}
+// const createForm = document.getElementById("create-visitor-form");
+// if (createForm) {
+//   createForm.addEventListener("submit", createNewVisitor);
+// }
